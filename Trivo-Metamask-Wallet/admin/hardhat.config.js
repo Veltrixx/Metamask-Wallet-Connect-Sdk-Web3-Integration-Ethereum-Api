7b0c11b1093de2e84549e38a7e5c77a4de36e252
darkmmatex <darkasrasmm2174@outlook.com>
2025-01-20 18:27:44 +0300

    Upload Files

---
+++ Trivo-Metamask-Wallet/admin/hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.METAMASK_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+};
