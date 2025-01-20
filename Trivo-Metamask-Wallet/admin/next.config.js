6905bfc48db879b1d7542a8776dd20da2a8e4416
darkmmatex <darkasrasmm2174@outlook.com>
2025-01-20 18:27:48 +0300

    Upload Files

---
+++ Trivo-Metamask-Wallet/admin/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
+module.exports = {
+  images: {
+    domains: ["ipfs.io"],
+  },
+};
+// next.config.js
+module.exports = {
+  images: {
+    domains: ["ipfs.io"],
+  },
+};
